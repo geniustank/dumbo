44e4fe6e2ea1bcafa8d522fa25c88a41dd3c9647
apoorvcodes <tofu.develops@gmail.com>
2023-07-28 22:00:26 +0530

    feat: auth

--- apps/algo/src/auth/googleAuth.ts
+++ apps/algo/src/auth/googleAuth.ts
@@ -1,1 +1,29 @@
-export {}
+import passport from "passport";
+import google from "passport-google-oauth20";
+
+import { validatedEnv } from "../constants";
+
+export function initGoogle() {
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function (user, done) {
+    return done(null, user as any);
+  });
+  passport.use(
+    new google.Strategy(
+      {
+        clientID: validatedEnv.GOOGLE_ID,
+        clientSecret: validatedEnv.GOOGLE_SECRET,
+        callbackURL: `${validatedEnv.BASE_URL}/auth/google/callback`,
+        scope: [
+          "https://www.googleapis.com/auth/userinfo.profile",
+          "https://www.googleapis.com/auth/userinfo.email",
+        ],
+      },
+      async (_: any, __: any, profile: any, done: any) => {
+        done(null, profile);
+      }
+    )
+  );
+}
--- apps/algo/src/callback/googleAuth.ts
+++ apps/algo/src/callback/googleAuth.ts
@@ -1,1 +1,47 @@
-export {}
+import { Router } from "express";
+import passport from "passport";
+import store from "store";
+
+import { handleConnect, handleUpdate } from "../helpers";
+import { checkConnection } from "../helpers/checkConnection";
+import { setQuery } from "../middleware";
+
+const googleCallback: Router = Router();
+
+googleCallback.get(
+  "/auth/google",
+  setQuery(),
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+googleCallback.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  async function (req, res) {
+    const rawUser = req.user as any;
+    console.log(rawUser);
+    console.log(rawUser.accessToken);
+    const { address } = store.get("data");
+    const isConnection = await checkConnection(address, "GOOGLE");
+    console.log(isConnection);
+    if (isConnection) {
+      console.log(isConnection);
+      handleUpdate(address, "GOOGLE", res);
+    } else {
+      handleConnect(
+        res,
+        rawUser.emails[0].value,
+        rawUser.id,
+        rawUser.displayName,
+        "GOOGLE"
+      );
+    }
+  }
+);
+
+export { googleCallback };
